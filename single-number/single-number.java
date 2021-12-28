e4b81b59bc86e83f46b3bdccf916e894698254b6
Rohan Patil <rohanpatil365@gmail.com>
2021-12-28 22:21:06 +0530

    Time: 1 ms (92.91%), Space: 38.6 MB (99.61%) - LeetHub

--- single-number/single-number.java
+++ single-number/single-number.java
@@ -1,22 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int n = nums.length;
-        int size = n/2 + 1;
-        int missing=-1;
-        HashSet<Integer> hs = new HashSet<Integer>(size);
-        for(int i=0;i<n;i++){
-            int num = nums[i];
-            if(hs.contains(num)){
-                hs.remove(num);
-            }
-            else{
-                hs.add(num);
-            }
+        
+        int xor = 0;
+        for(int i=0;i<nums.length;i++){
+            xor ^= nums[i];
         }
-        for(Integer num : hs){
-            missing = num;
-            break;
-        }
-        return missing;
+        return xor;
     }
 }
