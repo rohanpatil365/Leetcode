d42430ba412026ae7b7ee8c5af7bac9c732931c9
Rohan Patil <rohanpatil365@gmail.com>
2022-01-04 20:13:12 +0530

    Time: 227 ms (14.85%), Space: 65.3 MB (15.77%) - LeetHub

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -9,14 +9,18 @@ class Solution {
         int rootn = (int)Math.sqrt(n);
         rootn +=1;
         
-        int count = 0;
-        
-        for(int i=2;i<n;i++){
+        for(int i=2;i<rootn;i++){
             if(!notPrime[i]){
-                count++;
                 for(int j=2;j*i < n;j++){
                     notPrime[i*j] = true;
                 }
+            }
+        }
+        
+        int count = 0;
+        for(int i=2;i<n;i++){
+            if(!notPrime[i]){
+                count++;
             }
         }
         
